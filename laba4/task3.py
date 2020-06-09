cd74d211519f5d6eb5de59a5801efa18af10a431
gettmure <warcraft6500@gmail.com>
2020-06-09 23:03:35 +1000

    tilted af

--- laba4/task3.py
+++ laba4/task3.py
@@ -1,65 +1,36 @@
 import matplotlib.pyplot as plt
 
 
-class ODE:
-    _plot_is_created = False
-    _values_are_calculated = False
+def get_x(x, y, alpha, beta):
+    return (alpha - beta * y) * x
 
-    def __init__(self, initial_x, initial_y, alpha, beta):
-        self._x = initial_x
-        self._y = initial_y
-        self._alpha = alpha
-        self._gamma = alpha
-        self._beta = beta
-        self._delta = beta
-        self._x_values = [initial_x]
-        self._y_values = [initial_y]
 
-    def __get_x_value(self, x, y):
-        return (self._alpha - self._beta * y) * x
+def get_y(x, y, delta, gamma):
+    return (delta * x - gamma) * y
 
-    def __get_y_value(self, x, y):
-        return (self._delta * x - self._gamma) * y
 
-    def calculcate_values(self, step):
-        for i in range(0, 5):
-            x = self.__get_x_value(self._x, self._y) * step + self._x
-            y = self.__get_y_value(self._x, self._y) * step + self._y
-            self._x = self._x_values[i]
-            self._y = self._x_values[i]
-            self._x_values.append(x)
-            self._y_values.append(y)
-            print(self._x_values[i],  self._y_values[i])
-        self._values_are_calculated = True
-
-    def create_plot(self):
-        if (self._values_are_calculated):
-            plt.plot(self._x_values, self._y_values)
-            plt.xlabel('$x$')
-            plt.ylabel('$y$')
-            plt.title("ODE plot")
-            self._plot_is_created = True
-        else:
-            print("obama denied this operation!")
-
-    def show_plot(self):
-        if (not self._plot_is_created):
-            self.create_plot()
-        plt.show()
-
-    def save_plot(self, name, extension):
-        if (self._plot_is_created):
-            plt.savefig(f'{name}.{extension}')
-            plt.close()
-        else:
-            print("Plot doesn't exist!")
+def get_data(x, y, alpha, beta, delta, gamma, step):
+    data = [[], []]
+    for i in range(0, 1000000):
+        data[0].append(get_x(x, y, alpha, beta) * step + x)
+        data[1].append(get_y(x, y, delta, gamma) * step + y)
+        x = data[0][i]
+        y = data[1][i]
+    return data
 
 
 def main():
-    ode = ODE(1, 1, 0.29, 0.29)
-    ode.calculcate_values(0.001)
-    ode.create_plot()
-    ode.save_plot('plot', 'png')
+    y = 0
+    x = 1
+    step = 0.001
+    alpha = gamma = 0.29
+    beta = delta = 0.29
+
+    data = get_data(x, y, alpha, beta, delta, gamma, step)
+
+    plt.plot(data[0], data[1])
+    plt.savefig('plot.png')
+    plt.close()
 
 
 if __name__ == "__main__":
